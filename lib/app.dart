4254eaefd5dadc10210876698c1897b8209352bb
Hoang Truong <73890707+hoangankin2211@users.noreply.github.com>
2023-02-03 15:51:20 +0700

    use bloc instead of StatefulWidget

--- lib/app.dart
+++ lib/app.dart
@@ -1,40 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import './widget/index_widget.dart';
 
-class DashboardScreen extends StatefulWidget {
+class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
-  @override
-  State<DashboardScreen> createState() => _DashboardScreenState();
-}
-
-class _DashboardScreenState extends State<DashboardScreen> {
-  final List<Widget> widgets = [
-    const GetWidget(),
-    const PostWidget(),
+  static const List<Widget> widgets = [
+    GetWidget(),
+    PostWidget(),
   ];
-  int _currentIndex = 0;
-  void _opTapNavigationBar(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        unselectedItemColor: Colors.grey,
-        selectedItemColor: Colors.blue,
-        elevation: 5,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.widgets), label: "GET"),
-          BottomNavigationBarItem(icon: Icon(Icons.widgets), label: "POST"),
-        ],
-        currentIndex: _currentIndex,
-        onTap: _opTapNavigationBar,
+    return BlocProvider(
+      create: (context) => PositionBloc(const InitPositionState(0)),
+      child: BlocBuilder<PositionBloc, PositionState>(
+        builder: (context, state) => Scaffold(
+          bottomNavigationBar: BottomNavigationBar(
+            unselectedItemColor: Colors.grey,
+            selectedItemColor: Colors.blue,
+            elevation: 5,
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.widgets), label: "GET"),
+              BottomNavigationBarItem(icon: Icon(Icons.widgets), label: "POST"),
+            ],
+            currentIndex: state.currentPosition,
+            onTap: (value) {
+              context
+                  .read<PositionBloc>()
+                  .add(ChangingNewPosition(newPosition: value));
+            },
+          ),
+          body: Center(child: widgets[state.currentPosition]),
+        ),
       ),
-      body: Center(child: widgets[_currentIndex]),
     );
+  }
+}
+
+////////////////////STATE////////////////////
+abstract class PositionState {
+  final int currentPosition;
+  const PositionState(this.currentPosition);
+}
+
+class InitPositionState extends PositionState {
+  const InitPositionState(super.currentPosition);
+}
+
+class NewPoistionState extends PositionState {
+  const NewPoistionState(super.currentPosition);
+}
+
+////////////////////EVENT////////////////////
+abstract class PositionEvent {
+  const PositionEvent();
+}
+
+class ChangingNewPosition extends PositionEvent {
+  final int newPosition;
+
+  ChangingNewPosition({required this.newPosition});
+}
+
+//////////////////////BLOC//////////////////////
+class PositionBloc extends Bloc<PositionEvent, PositionState> {
+  PositionBloc(InitPositionState? initialState)
+      : super(initialState ?? const InitPositionState(0)) {
+    on<ChangingNewPosition>(handleChangingPosition);
+  }
+
+  void handleChangingPosition(
+      ChangingNewPosition event, Emitter<PositionState> emit) {
+    emit(NewPoistionState(event.newPosition));
   }
 }
